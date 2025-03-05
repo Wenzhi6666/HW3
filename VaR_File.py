84883119f5f82312fd99cd81049b7c54c6ba2021
Wenzhi6666 <ws2760@columbia.edu>
2025-03-05 16:27:37 -0500

    Update VaR_File.py

--- VaR_File.py
+++ VaR_File.py
@@ -1,6 +1,11 @@
-import numpy as np
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+'''
 def VaR(r, alpha):
+    
     # This function returns the left tail value
     # alpha = risk level
     # r = an array of stock returns
@@ -8,3 +13,8 @@ def VaR(r, alpha):
 
     out = .01 # Calculate the percentile
     return abs(out)  # Return the absolute value of the calculated percentile
+'''
+    def VaR(r, confidence, principal=1):
+    varpercentile = np.percentile(r, (1-confidence)*100)  
+    out = abs(principal * varpercentile)  
+    return out
